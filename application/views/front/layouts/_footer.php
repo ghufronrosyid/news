854a1324930b43905aecf8e6055967e4dfe06a3f
ghufronrosyid <ghufronrosyid99@email.com>
2023-06-13 20:01:57 +0700

    first commit

---
+++ application/views/front/layouts/_footer.php
@@ -0,0 +1,59 @@
+<!--================ start footer Area  =================-->	
+<footer class="footer-area mt-5">
+   <div class="container">
+      <div class="row f_widgets_inner">
+         <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="single-footer-widget ab_widgets">
+               <div class="f_title">
+                  <h3>GNews</h3>
+               </div>
+               <p>GNews merupakan portal berita kekinian</p><br>
+               <h3>Chuaksszz..</h3>
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="single-footer-widget">
+                  <div class="f_title">
+                  <h3>Quick Links</h3>
+                  </div>
+                  <div class="row">
+                     <div class="col-6">
+                        <h5 style="color: white">Kategori</h5>
+                        <ul class="list mt-3">
+                           <?php foreach($category as $c) : ?>
+                              <li><a href="<?= base_url("blog/category/$c->slug") ?>"><?= $c->category_name ?></a></li>
+                           <?php endforeach ?>
+                        </ul>
+                     </div>
+                     <div class="col-6">
+                        <h5 style="color: white">Info</h5>
+                        <ul class="list">
+                              <li><a href="<?= base_url('contact') ?>">Tentang Kami</a></li>
+                        </ul>
+                     </div>										
+                  </div>							
+            </div>
+         </div>
+         <div class="col-lg-4 col-md-6">
+            <div class="single-footer-widget m_news_widgets">
+               <div class="f_title">
+                  <h3>Social Networks</h3>
+               </div>
+               <div class="col-lg-4 col-md-4 footer-social icon mx-auto">
+                  <a href="#"><i class="fa fa-facebook"></i></a>
+                  <a href="#"><i class="fa fa-twitter"></i></a>
+                  <a href="#"><i class="fa fa-youtube-play"></i></a>
+               </div>
+            </div>
+         </div>	
+      </div>
+      <div class="row footer-bottom d-flex justify-content-between align-items-center">
+         <div class="col-lg-12">
+            <div class="f_boder"></div>
+         </div>
+         <p class="col-lg-8 col-md-8 footer-text">
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#" target="_blank">POH</a></p>
+      </div>
+   </div>
+</footer>
+<!--================ End footer Area  =================-->
